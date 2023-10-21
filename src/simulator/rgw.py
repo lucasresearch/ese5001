44444065aec91701d815c11e12eb8eb81ab0e2eb
Lucas <paracuentas.de.lucas@gmail.com>
2023-10-21 18:52:31 -0500

    inserted goal state

--- src/simulator/rgw.py
+++ src/simulator/rgw.py
@@ -9,10 +9,13 @@ class GridWorld:
     def __init__(self,height:int=10,width:int=10, p:float=0.1) -> None:
         self.height = height
         self.width = width
-        self.grid = np.random.choice(a=[False, True], size=(height, width), p=[1-p, p])
+        self.grid = np.random.choice(a=[0, 1], size=(height, width), p=[1-p, p])
+        self.goalRowIndex = np.random.randint(height)
+        self.goalColIndex = np.random.randint(width)
+        self.grid[self.goalRowIndex][self.goalColIndex] = 2
         return
     def plot(self) -> None:
-        plt.imshow(self.grid, cmap='Reds',  interpolation='nearest')
+        plt.imshow(self.grid, cmap='brg',  interpolation='nearest')
         plt.show()
         return
     def print():
