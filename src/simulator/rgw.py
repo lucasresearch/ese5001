04ff79b08257a3b3aad06495ceb36ddcb33708e4
Lucas <paracuentas.de.lucas@gmail.com>
2023-10-21 11:45:08 -0500

    added pyplot

--- src/simulator/rgw.py
+++ src/simulator/rgw.py
@@ -4,14 +4,11 @@ the code necesary to initialize a GridWorld with obstacles
 in random places
 """
 import numpy as np
-from scipy import sparse
+import matplotlib.pyplot as plt
 class GridWorld:
     def __init__(self,height:int=10,width:int=10,density:float=0.1) -> None:
         self.height = height
         self.width = width
-
-
-        # Init Random Grid With FAST Code stolen from
-        # https://stackoverflow.com/questions/43528637/create-large-random-boolean-matrix-with-numpy
-        
+        self.grid = np.random.choice(a=[False, True], size=(height, width), p=[density, 1-density])
         return
+    def plot
